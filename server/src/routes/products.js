194c7481d564ad07d01fa1b6ebc781f8f80d4107
Feli <srgatoiscool@gmail.com>
2025-01-21 10:27:10 -0500

    Add Swagger documentation for products route; include summary and response details

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Obtiene la lista de productos
+ *     responses:
+ *       200:
+ *         description: Lista de productos obtenida exitosamente
+ */
 router.get('/', productController.getProducts);
 
 module.exports = router;
