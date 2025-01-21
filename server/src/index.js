4a81fc8ab7f5d2561c25c359033d8982e1dd3408
Feli <srgatoiscool@gmail.com>
2025-01-21 14:11:15 -0500

    Update API documentation route from '/api/api-docs' to '/api/docs' for clarity

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,7 @@ app.use(
     })
 );
 
-app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyParser.json());
 
